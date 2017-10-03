c8dff7dd0a56130e00ebc4139f0db725f68557cd
Yanny Castrillon <yannyandres117@gmail.com>
2017-10-02 22:20:49 -0700

    Manage and build the counter component with all the total of guests, the ones that are coming to the event and the ones who hasn't confirmed yet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GuestList from './GuestList.js'
 import './App.css';
+import Counter from './Counter';
 
 class App extends Component {
   state = {
@@ -58,6 +59,11 @@ class App extends Component {
 
   getTotalInvited = () => this.state.guests.length
 
+  getAttendingGuests = () =>
+    this.state.guests.reduce((total, guest) => {
+      return guest.isConfirmed ? total + 1 : total
+    }, 0)
+
   // Sets the current name when editing.
   setNameAt = (name, indexToChange) => {
     this.setState({
@@ -106,6 +112,9 @@ class App extends Component {
   }
 
   render() {
+    const totalInvited = this.getTotalInvited()
+    const numberAttending = this.getAttendingGuests()
+    const numberUnconfirmed = totalInvited - numberAttending
     return (
       <div className="App">
         <header>
@@ -135,22 +144,11 @@ class App extends Component {
               /> Hide those who haven' /t responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+          <Counter
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed}
+          />
           <GuestList
             guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
